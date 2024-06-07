9cb94a2b96bd410a4c8eb50da8c338bab7acefff
Texashokies <43481909+Texashokies@users.noreply.github.com>
2024-06-07 13:12:15 -0500

    Fixed padding location

--- src/js/wrg-index.js
+++ src/js/wrg-index.js
@@ -69,7 +69,7 @@ customElements.define(
       }
 
       return html`
-        <div id='tabs'>
+        <div id="tabs" class="pb-3">
             <button onClick="clickTab('all')">All</button>
             <button onClick="clickTab('store')">Store</button>
             <button onClick="clickTab('community')">Community</button>
